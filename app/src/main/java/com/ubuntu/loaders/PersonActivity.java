aaac57aa268f1a9c2cc7c5f0797d68d15fa1df9b
PuniCharana <punicharana@gmail.com>
2017-07-16 00:55:46 +0530

    added OnItemClickListener to listView

--- app/src/main/java/com/ubuntu/loaders/PersonActivity.java
+++ app/src/main/java/com/ubuntu/loaders/PersonActivity.java
@@ -8,8 +8,10 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,6 +49,16 @@ public class PersonActivity extends AppCompatActivity {
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         personListView.setAdapter(personAdapter);
+
+        // Set an item click listener on the ListView.
+        personListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Person person = personAdapter.getItem(i);
+
+                Toast.makeText(getApplicationContext(), person.getFname()+" "+person.getLname(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
