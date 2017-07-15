b3aedcdbe8bed04de726542b63f23d82c641ca65
PuniCharana <punicharana@gmail.com>
2017-07-15 20:19:32 +0530

    added not found text when list is empty along with loading indicator

--- app/src/main/java/com/ubuntu/loaders/PersonActivity.java
+++ app/src/main/java/com/ubuntu/loaders/PersonActivity.java
@@ -2,7 +2,9 @@ package com.ubuntu.loaders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,10 @@ import java.util.List;
 public class PersonActivity extends AppCompatActivity {
 
     private PersonAdapter personAdapter;
+
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +24,9 @@ public class PersonActivity extends AppCompatActivity {
 
         // Find a reference to the {@link ListView} in the layout
         ListView personListView = (ListView) findViewById(R.id.list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        personListView.setEmptyView(mEmptyStateTextView);
 
 
         // Create a new adapter that takes an empty list of earthquakes as input
@@ -32,7 +41,15 @@ public class PersonActivity extends AppCompatActivity {
     }
 
     private void addDummyData(){
-        List<Person> personList = new ArrayList<Person>();
+
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No person found."
+        mEmptyStateTextView.setText(R.string.no_person);
+
+        List<Person> personList = new ArrayList<>();
         for (int i=1; i <= 20; i++) {
             Person person = new Person("Puni", "Charana", "Male", 1234567890098L);
             personList.add(person);
@@ -41,3 +58,4 @@ public class PersonActivity extends AppCompatActivity {
         personAdapter.addAll(personList);
     }
 }
+
